1022bbce9213492d10c91a0ffddeccadbafa3a1e
arnaldocorzo <arnaldo.corzo@prescriberpoint.com>
2024-03-05 13:07:42 -0300

    adding solutions

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Aspose.Words;
+using Aspose.Words.Fields;
+
+
+Console.WriteLine("Starting process");
+
+var document = new Document(@".\Docs\Doc1.docx");
+var builder = new DocumentBuilder(document);
+
+var sectionIndex = 0;
+foreach (Section section in document.Sections)
+{
+    section.PageSetup.RestartPageNumbering = false;
+    builder.MoveToSection(1);
+
+    var footer = section.HeadersFooters[HeaderFooterType.FooterPrimary];
+    var footer2 = section.HeadersFooters[HeaderFooterType.FooterFirst];
+    var footer3 = section.HeadersFooters[HeaderFooterType.FooterEven];
+    var hasPageNumber = ContainsPageNumber(footer);
+    if (!hasPageNumber)
+    {
+        builder.MoveToSection(sectionIndex);
+        builder.MoveTo(footer.LastChild);
+        builder.InsertField(FieldType.FieldPage, true);
+    }
+
+    sectionIndex++;
+}
+    
+
+document.Save(@".\Docs\out.docx");
+
+Console.WriteLine("Process ends...");
+
+static bool ContainsPageNumber(HeaderFooter footer)
+{
+    var fields = footer.GetChildNodes(NodeType.FieldStart, true);
+
+    return fields.Any(x => ((FieldStart)x).FieldType == FieldType.FieldPage);
+}
---
+++ Test2/Test2/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Aspose.Words;
+using Aspose.Words.Tables;
+
+Console.WriteLine("Starting process");
+
+var document = new Document(@".\Docs\Doc2.docx");
+
+var newDoc = (Document)document.Clone(false);
+var builder = new DocumentBuilder(newDoc);
+
+var table = document.FirstSection.GetChild(NodeType.Table, 0, true) as Table;
+
+if (table is null)
+{
+    Console.WriteLine("No Table found");
+    return;
+}
+
+NodeImporter importer = new NodeImporter(document, newDoc, ImportFormatMode.KeepSourceFormatting);
+
+var sectionIndex = 0;
+var lastSectionIndex = table.Rows.Count - 1;
+foreach (Row row in table.Rows)
+{
+    var cellIndex = 0;
+    foreach (Cell cell in row.Cells)
+    {
+        if (cellIndex == 0)
+        {
+            var destNode = importer.ImportNode(cell.FirstParagraph, true);
+            newDoc.Sections[sectionIndex].Body.AppendChild(destNode);
+        }
+        else
+        {
+            var nodes = cell.GetChildNodes(NodeType.Paragraph, true);
+            foreach (var node in nodes)
+            {
+                var destNode = importer.ImportNode(node, true);
+                newDoc.Sections[sectionIndex].Body.AppendChild(destNode);
+            }
+        }
+        cellIndex++;
+    }
+
+    if (sectionIndex < lastSectionIndex)
+    {
+        builder.MoveToSection(sectionIndex);
+        builder.MoveTo(newDoc.Sections[sectionIndex].Body.LastChild);
+        builder.InsertBreak(BreakType.SectionBreakNewPage);
+
+        sectionIndex++;
+    }
+}
+
+newDoc.Save(@".\Docs\out.docx");
+
+Console.WriteLine("Process ends...");
